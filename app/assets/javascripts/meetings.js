cbc08860b60d144962acdac85c1e0c692e7570ab
Peter Jang <peter.x.jang@gmail.com>
2017-04-30 11:48:02 -0500

    Add name filter for dynamic search

--- app/assets/javascripts/meetings.js
+++ app/assets/javascripts/meetings.js
@@ -3,12 +3,20 @@ document.addEventListener("DOMContentLoaded", function(event) {
   var app = new Vue({
     el: '#app',
     data: {
-      meetings: []
+      meetings: [],
+      nameFilter: ''
     },
     mounted: function() {
       $.get('/api/v1/meetings', function(responseData) {
         this.meetings = responseData;
       }.bind(this));
+    },
+    computed: {
+      modifiedMeetings: function() {
+        return this.meetings.filter(function(meeting) {
+          return meeting.name.toLowerCase().indexOf(this.nameFilter.toLowerCase()) !== -1;
+        }.bind(this));
+      }
     }
   });
 });
