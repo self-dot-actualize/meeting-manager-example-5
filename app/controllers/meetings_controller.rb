e833422f040ba7b9b7bc614ff9e11735204d42b8
Peter Jang <peter.x.jang@gmail.com>
2017-04-30 11:41:42 -0500

    Rewrite meetings index page using Vue.js

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,10 +1,5 @@
 class MeetingsController < ApplicationController
   def index
-    if params[:tag]
-      @meetings = Tag.find_by(name: params[:tag]).meetings
-    else
-      @meetings = Meeting.all
-    end
     render "index.html.erb"
   end
 
