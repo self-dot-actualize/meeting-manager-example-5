10cf7dd7e73b5349137acf47881aa43f27dc7c53
Peter Jang <peter.x.jang@gmail.com>
2017-04-30 09:24:08 -0500

    Add meetings new and create actions

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -8,6 +8,22 @@ class MeetingsController < ApplicationController
     render "index.html.erb"
   end
 
+  def new
+    render "new.html.erb"
+  end
+
+  def create
+    @meeting = Meeting.new(
+      name: params[:name],
+      address: params[:address],
+      start_time: params[:start_time],
+      end_time: params[:end_time],
+      notes: params[:notes],
+    )
+    @meeting.save
+    redirect_to "/meetings/#{@meeting.id}"
+  end
+
   def show
     @meeting = Meeting.find_by(id: params[:id])
     render "show.html.erb"
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get '/' => 'meetings#index'
 
   get '/meetings' => 'meetings#index'
+  get '/meetings/new' => 'meetings#new'
+  post '/meetings' => 'meetings#create'
   get '/meetings/:id' => 'meetings#show'
 end
