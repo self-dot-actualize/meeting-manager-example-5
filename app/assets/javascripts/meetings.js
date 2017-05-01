9429a7abee0233c2171d1c9a699171a26eae519b
Peter Jang <peter.x.jang@gmail.com>
2017-04-30 19:30:36 -0500

    Add buttons to sort by different attributes

--- app/assets/javascripts/meetings.js
+++ app/assets/javascripts/meetings.js
@@ -4,7 +4,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
     el: '#app',
     data: {
       meetings: [],
-      nameFilter: ''
+      nameFilter: '',
+      sortAttribute: 'address',
+      sortAscending: false
     },
     mounted: function() {
       $.get('/api/v1/meetings', function(responseData) {
@@ -13,9 +15,28 @@ document.addEventListener("DOMContentLoaded", function(event) {
     },
     computed: {
       modifiedMeetings: function() {
-        return this.meetings.filter(function(meeting) {
+        var filteredMeetings = this.meetings.filter(function(meeting) {
           return meeting.name.toLowerCase().indexOf(this.nameFilter.toLowerCase()) !== -1;
         }.bind(this));
+        var sortedMeetings = filteredMeetings.sort(function(m1, m2) {
+          console.log(m1[this.sortAttribute], m2[this.sortAttribute]);
+          if (this.sortAscending) {
+            return m1[this.sortAttribute].localeCompare(m2[this.sortAttribute]);
+          } else {
+            return m2[this.sortAttribute].localeCompare(m1[this.sortAttribute]);
+          }
+        }.bind(this));
+        return sortedMeetings;
+      }
+    },
+    methods: {
+      changeSortAttribute: function(inputAttribute) {
+        if (inputAttribute !== this.sortAttribute) {
+          this.sortAscending = true;
+        } else {
+          this.sortAscending = !this.sortAscending;
+        }
+        this.sortAttribute = inputAttribute;
       }
     }
   });
